cef289bb944cd3456e06722e41f8dffc2d6f6bdd
Aaron <562540603@qq.com>
2016-08-06 21:12:09 +0800

    提交

--- ZZFileManager/ZZFileManager/ZZFileCache.m
+++ ZZFileManager/ZZFileManager/ZZFileCache.m
@@ -96,7 +96,10 @@ static const NSInteger kDefaultCacheMaxCacheAge = 60 * 60 * 24 * 7; // 1 week
     unsigned char r[CC_MD5_DIGEST_LENGTH];
     CC_MD5(str, (CC_LONG)strlen(str), r);
     NSString *filename = [NSString stringWithFormat:@"%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x", r[0], r[1], r[2], r[3], r[4], r[5], r[6], r[7], r[8], r[9], r[10], r[11], r[12], r[13], r[14], r[15]];
-    
+    NSString *exestr = [key pathExtension];
+    if (exestr.length) {
+        filename = [NSString stringWithFormat:@"%@.%@", filename, exestr];
+    }
     return filename;
 }
 
